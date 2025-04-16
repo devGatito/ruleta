e4acb99e48006156093a93c3e6a73b379423b721
Elias Jimenez <eliasmisaelsotojimenez1@gmail.com>
2025-04-16 10:20:05 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,          // ← necesario
-  port: process.env.DB_PORT,          // ← necesario para evitar socket local
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT), // <-- esto es importante
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
